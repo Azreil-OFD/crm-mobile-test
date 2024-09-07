51dd3f3260e6276d9005397d906fcdb557017194
Azreil-OFD <fantom2413@gmail.com>
2024-09-07 21:45:23 +0500

    initial commit

---
+++ src-tauri/gen/android/app/src/main/java/ru/evil_chan/crm_test/MainActivity.kt
@@ -0,0 +1,3 @@
+package ru.evil_chan.crm_test
+
+class MainActivity : TauriActivity()
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// TODO создать функцию 2
+createApp(App).mount("#app");
